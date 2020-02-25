4fc05d4b7287cfa9cbd8f82f1ca5799bcc884676
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-02-25 17:03:30 +0900

    jQuery-setting

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,20 +1,30 @@
-$(function () {
-  var buildPrompt = `<option value>---</option>`
-  var buildHtmlOption = function (cat) {
-    var option = `<option value ="${cat.id}">${cat.name}</option>`
-    return option
-  }
-  var buildInputFile = function (num) {
-    var input = `<input data-id="${num}" type="file" name="product[images_attributes][${num}][image_url]" id="product_images_attributes_${num}_image_url">`
-    return input
-  }
-  var product_img = $('.product_image')
-  
-    product_img.parents('.sell__image-select__ul-text').css('opacity', 1);
-  product_img.parent().next('.btn-box').css('display', 'block')
+// $(document).ready(function() {
+//   $("button").on('click', function(){
+//     $(this).text("Hello");
+//     $(this).css("background-color", "#686869"); 
+//     $(this).css("color", "white");
+//     $(this).off('click');
+//   });
+// });
 
 
-  
-  
-
+$(document).ready(function() {
+  $('button').each(function() {
+    var elements = $(this);
+    var count = 0;
+    var defaultText = elements.text();
+    elements.click(function() {
+      if ( count === 0 ){
+        elements.text('chose');
+        elements.css("background-color", "#686869"); 
+        elements.css("color", "white");
+        count = 1;
+      } else{
+        elements.text(defaultText);
+        $(this).css("background-color", "whitesmoke"); 
+        $(this).css("color", "#686869");
+        count = 0;
+      }
+    });
+  });
 });
