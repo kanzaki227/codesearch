9a51b52f50dd9cafc8a4beb7c9edd44804457875
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-02-28 15:49:18 +0900

    classname-update

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -9,16 +9,34 @@
 
 
 $(document).ready(function() {
-  $('button').each(function() {
+  $('.btnC').each(function() {
     var elements = $(this);
     var count = 0;
     var defaultText = elements.text();
     elements.click(function() {
       if ( count === 0 ){
-        elements.text('chose').css("background-color", "#686869").css("color", "white");
+        elements.text('chose!!').css("background-color", "#686869").css("color", "white").css("text-align", "center");
+        $("img").css("display", "inline");
         count = 1;
       } else{
         elements.text(defaultText).css("background-color", "whitesmoke").css("color", "#686869");
+        $("img").css("display", "none");
+        count = 0;
+      }
+    });
+  });
+  $('.btnCsha').each(function() {
+    var elements = $(this);
+    var count = 0;
+    var defaultText = elements.text();
+    elements.click(function() {
+      if ( count === 0 ){
+        elements.text('chose!!').css("background-color", "#686869").css("color", "white").css("text-align", "center");
+        $("img").css("display", "inline");
+        count = 1;
+      } else{
+        elements.text(defaultText).css("background-color", "whitesmoke").css("color", "#686869");
+        $("img").css("display", "none");
         count = 0;
       }
     });
